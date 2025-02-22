0e58928100b9fe4209b092abe31faf664cc75e65
alireza <spsaefan>
2025-02-22 12:46:25 +0330

    Initial commit

---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+print("Numpy Version:", np.__version__)
+print("Pandas Version:", pd.__version__)
+print("TensorFlow Version:", tf.__version__)
